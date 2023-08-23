21a63f1b097d0278f041d98d3b857cf3b9d28bb1
Azumi Suzuki <azumi.suzuki@tier4.jp>
2023-08-23 13:35:28 +0900

    WIP
    Signed-off-by: Azumi Suzuki <azumi.suzuki@tier4.jp>

--- scripts/hw_connectivity_check.sh
+++ scripts/hw_connectivity_check.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
 
-# GCP02 NIC ID: ***
+echo "Checking USB NIC connection..."
+
+# GCP02 NIC ID: enxc436c0eaa070
 # GCP03 NIC ID: enx00e04c0a0ba6
 
 # get vehicle id from NIC device id
 declare -A NIC_DEV=(
+    ["enxc436c0eaa070"]="GCP02"
     ["enx00e04c0a0ba6"]="GCP03"
 )
 
@@ -17,8 +20,9 @@ do
 done
 
 if [ -z $VEHICLE_ID ]; then
-  echo "USB_NIC has not connected."
+  echo "  Error: Specified USB NIC has not connected."
   exit 1
 fi
 
-echo "VEHICLE_ID = $VEHICLE_ID"
+echo "  VEHICLE_ID = $VEHICLE_ID"
+echo "  Check OK."
--- scripts/run_autoware_on_vehicle.sh
+++ scripts/run_autoware_on_vehicle.sh
@@ -1,7 +1,59 @@
-#!/bin/bash -e
+#!/bin/bash
 
-source vehicle.env
+### USB NIC connection check and get VEHICLE_ID
+declare -A NIC_DEV=(    #Specified USB NIC ID
+    ["enxc436c0eaa070"]="GCP02"
+    ["enx00e04c0a0ba6"]="GCP03"
+)
 
-MAP_PATH="/home/autoware/autoware/map/data"
+for DEV in `find /sys/devices -name net | grep -v virtual`
+do
+  DEV_NAME=("$(ls --quoting-style=shell $DEV)")
+  if [ -n "${NIC_DEV[$DEV_NAME]}" ]; then
+    export VEHICLE_ID="${NIC_DEV[$DEV_NAME]}"   # Export VEHICLE_ID
+  fi
+done
+
+if [ -z $VEHICLE_ID ]; then
+  echo "  Error: Specified USB NIC has not connected."
+  exit 1
+fi
+
+echo "USB NIC connection OK."
+echo "VEHICLE_ID = $VEHICLE_ID"
+
+
+### Lidar connection check
+/bin/ping 192.168.1.201 -w 1 -c 1 >> /dev/null
+if [ $? == 0 ]; then
+  echo "Velodyne connection OK."
+  exit 1
+else
+  echo "Error: Please check network settings and vehicle power is ON."
+  exit 1
+fi
+
+
+### CAN interface connection check
+# if can0 does not exist, configure can0
+set -e
+echo "Configuring CAN interface..."
+sudo ip link set can0 type can bitrate 500000
+sudo ip link set can0 txqueuelen 500000
+sudo ip link set can0 up
+set +e
+
+# candump can0 check, get data ?
+#   if no: error, please check vehicle power is ON, exit...
+#   if yes: OK!
+
+### set environment
+export VEHICLE_MODEL=ymc_golfcart
+export SENSOR_MODEL=aip_aichallenge
+export MAP_PATH="/home/autoware/autoware/map/data"
+
+# echo env var
+echo "Launch autoware."
+# wait 1 sec.
 
 ros2 launch autoware_launch autoware.launch.xml vehicle_model:=$VEHICLE_MODEL sensor_model:=$SENSOR_MODEL map_path:=$MAP_PATH
